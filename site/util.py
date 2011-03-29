151a16940e009a5388518f0349264ee640bf1819
David Krisch <david.krisch@yahoo.com>
2011-03-28 21:59:14 -0700

    Getting bill_model_to_forms working.

--- site/util.py
+++ site/util.py
@@ -31,18 +31,29 @@ def bill_model_to_forms(bill_model):
 			MonthlyRecurrenceForm, YearlyRecurrenceForm
 
 	forms = {}
-	forms['bill_form'] = BillForm(instance=bill_model)	
 	recurrence = bill_model.get_recurrence()
+
 	if recurrence:
-		forms['recur_freq_form'] = RecurFreqForm({'repeats': recurrence.frequency}),
+		forms['bill_form'] = BillForm(initial={'does_repeat': True}, instance=bill_model)	
+		forms['recur_freq_form'] = RecurFreqForm({'repeats': recurrence.frequency})
 		if recurrence.frequency == 'daily':
 			forms['daily_recurrence_form'] = DailyRecurrenceForm()
 		elif recurrence.frequency == 'weekly':
-			forms['weekly_recurrence_form'] = WeeklyRecurrenceForm(repeat_on=recurrence.byweekday)
+			forms['weekly_recurrence_form'] = WeeklyRecurrenceForm(initial={'repeat_on': recurrence.byweekday})
 		elif recurrence.frequency == 'monthly':
-			forms['monthly_recurrence_form'] = MonthlyRecurrenceForm({'repeat_by': None})
+			repeat_by = None
+			if recurrence.bymonthday:
+				repeat_by = 'day_of_month'
+			else:
+				repeat_by = 'day_of_week'
+
+			forms['monthly_recurrence_form'] = \
+					MonthlyRecurrenceForm(initial={'repeat_every_monthly': recurrence.interval, 'repeat_by': repeat_by})
 		elif recurrence.frequency == 'yearly':
 			forms['yearly_recurrence_form'] = YearlyRecurrenceForm()
+	else:
+		forms['bill_form'] = BillForm(instance=bill_model)	
+
 
 	return forms
 
